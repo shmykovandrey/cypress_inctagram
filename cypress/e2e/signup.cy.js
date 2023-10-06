2328d4c6f10a36757c32ce4f8aced632accd4960
Feomatar Feomatar <shmykovadnrey88@gmail.com>
2023-10-06 20:47:28 +0300

    feat: add fixtures and alias

--- cypress/e2e/signup.cy.js
+++ cypress/e2e/signup.cy.js
@@ -1,30 +1,37 @@
 ///<reference types="cypress" />
 
 describe("Sign up", () => {
-  it("should do register user", () => {
+  beforeEach(() => {
     cy.visit("/");
+  });
 
+  it("should do register user", () => {
     cy.get('.navbar a[href$="/register"]').click();
     cy.url().should("include", "/#/register");
 
-    cy.get(".auth-page h1").should("have.text", "Sign up");
-    cy.get(".auth-page form").should("be.visible");
+    cy.get(".auth-page").as("registerPage");
+    cy.get("@registerPage").find("h1").should("have.text", "Sign up");
+    cy.get("@registerPage")
+      .find("form")
+      .should("be.visible")
+      .as("registerForm");
 
     const rnd = Math.round(Math.random() * 8999) + 1000;
 
-    const username = "user_" + rnd;
+    const username = "userp_" + rnd;
     const email = username + "@gmail.com";
-    cy.get(".auth-page form input[ng-model$=username]").type(username);
-    cy.get(".auth-page form input[ng-model$=email]").type(email);
-    cy.get(".auth-page form input[ng-model$=password]").type("xyzXYZ123_");
-    cy.get(".auth-page form button[type=submit]").click();
+
+    cy.get("@registerForm").find("input[ng-model$=username]").type(username);
+    cy.get("@registerForm").find("input[ng-model$=email]").type(email);
+    cy.get("@registerForm")
+      .find("input[ng-model$=password]")
+      .type("xyzXYZ123_");
+    cy.get("@registerPage").find("button[type=submit]").click();
 
     cy.get(".navbar").should("contain.text", username);
   });
 
   it("should do login user", () => {
-    cy.visit("/");
-
     cy.get('.navbar a[href$="/login"]').click();
     cy.url().should("include", "/#/login");
 
@@ -42,8 +49,6 @@ describe("Sign up", () => {
 
   it("should do logout user", () => {
     const username = "test_anton";
-
-    cy.visit("/");
 
     // for login
     cy.get('.navbar a[href$="/login"]').click();
